f563a90e6f3dcf51d2254ed49c8bad3902362a14
synnestorm <synne.storm@gmail.com>
2026-03-27 14:29:59 +0100

    feat: product-specific.js

---
+++ js/product-specific.js
@@ -0,0 +1,5 @@
+// specific product page - fetching the prouct which is clicked on
+// by user
+
+const productCard = document.querySelector("#product-card");
+const apiUrl = "https://v2.api.noroff.dev/online-shop";
