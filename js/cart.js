96b096135334a872c701615ae08204dee2542653
synnestorm <synne.storm@gmail.com>
2026-04-03 01:32:11 +0200

    feat: cart functionality implemented, with function shoppingCart

---
+++ js/cart.js
@@ -0,0 +1,66 @@
+const containerCart = document.querySelector("#cart")
+const productsCart = document.querySelector(".products-cart")
+let cart = []
+try {
+    cart = JSON.parse(localStorage.getItem("cart")) || []
+} catch(error) {
+    console.error("Could not load cart", error)
+    cart = []
+}
+
+function shoppingCart() {
+    containerCart.innerHTML = ""
+    if(cart.length === 0) {
+        const emptyCart = document.createElement("p")
+        emptyCart.className = "empty-cart"
+        emptyCart.textContent = "Your shopping cart is currently empty:("
+        containerCart.appendChild(emptyCart)
+        productsCart.textContent = ""
+        return
+    }
+    let total = 0
+    cart.forEach((item, index) => {
+        if (!item || !item.price || !item.quantity) return;
+        total += item.price * item.quantity;
+        const displayProducts = document.createElement("div")
+        displayProducts.className = "display-products"
+        const image = document.createElement("img")
+        image.className = "product-image"
+        image.src = item.image
+        image.alt = item.title
+        const title = document.createElement("h2")
+        title.className = "product-title"
+        title.textContent = item.title
+        const price = document.createElement("span")
+        price.className = "product-price"
+        price.textContent = `${item.price} NOK`
+        const removeBtn = document.createElement("button")
+        removeBtn.className = "remove-btn"
+        removeBtn.textContent = "Remove product"
+        removeBtn.addEventListener("click", () => removeFromCart(item.id))
+        displayProducts.appendChild(image)
+        displayProducts.appendChild(title)
+        displayProducts.appendChild(price)
+        displayProducts.appendChild(removeBtn)
+        containerCart.appendChild(displayProducts)
+    })
+    const totalPrice = document.createElement("div")
+    totalPrice.className = "total-price"
+    totalPrice.textContent = `Total price: ${total.toFixed(2)} NOK`
+    containerCart.appendChild(totalPrice)
+    const continueBtn = document.createElement("button")
+    continueBtn.className = "continue-btn"
+    continueBtn.textContent = "Continue to checkout"
+    continueBtn.addEventListener("click", () => {
+        window.location.href = "../checkout/index.html"
+        })
+    containerCart.appendChild(continueBtn)
+}
+
+function removeFromCart(productId) {
+    cart = cart.filter(item => !(item.id === productId))
+    localStorage.setItem("cart", JSON.stringify(cart))
+    shoppingCart()
+}
+
+shoppingCart()
