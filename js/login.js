da76f9695f9d7e8de07bfcbdc11de5dc9123bbb5
synnestorm <synne.storm@gmail.com>
2026-05-10 17:39:26 +0200

    bugfix: removed validateForm, validateEmail, validatePassword functions

--- js/login.js
+++ js/login.js
@@ -1,9 +1,6 @@
 const loginForm = document.getElementById("loginForm")
 const emailInput = document.getElementById("email")
 const password = document.getElementById("password")
-const emailError = document.getElementById("emailError")
-const nameError = document.getElementById("nameError")
-const passwordError = document.getElementById("passwordError")
 const result = document.getElementById("result")
 const loginUrl = "https://v2.api.noroff.dev/auth/login";
 
@@ -12,42 +9,6 @@ loader.className = "loader"
 loader.id = "loader"
 loader.style.display = "none"
 document.body.appendChild(loader)
-
-function showError(el, message) {
-  el.innerHTML = message;
-}
-
-function clearError(el) {
-  el.innerHTML = "";
-}
-
-function validateEmail(enteredEmail) {
-if (!user) {
-showError(emailError, "No user found.");
-return false;
-}
-  if (enteredEmail !== user.email) {
-    showError(emailError, "Incorrect Email.");
-    return false;
-  }
-  clearError(emailError);
-  return true;
-}
-
-function validatePassword(enteredPassword) {
-  if (enteredPassword !== user.password) {
-    showError(passwordError, "Incorrect password.");
-    return false;
-  }
-  clearError(passwordError);
-  return true;
-}
-
-function validateForm(enteredEmail, enteredPassword) {
-  let okEmail = validateEmail(enteredEmail);
-  let okPassword = validatePassword(enteredPassword);
-  return okEmail && okPassword;
-}
 
 loginForm.addEventListener("submit", async function (event) {
   event.preventDefault();
@@ -88,15 +49,10 @@ async function loginUser(email, password) {
       })
     });
     if (!response.ok) {
-      /* This is for debugging:
-      const errorData = await response.json();
-      console.error("API error:", JSON.stringify(errorData, null, 2));
-      */
       return false;
     }
     const data = await response.json();
     localStorage.setItem("authToken", data.data.accessToken);
-    // debugging: console.log("Login response:", data);
     return true;
   } catch (error) {
     console.error("Failed to fetch authentication.", error)
